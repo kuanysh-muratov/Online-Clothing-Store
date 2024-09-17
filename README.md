aa6a196d9546a818bbf3d73f2afdc3dff9735cd6
Kuanysh Muratov <kuanysh.muratov.05@gmail.com>
2024-09-17 23:34:11 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Online-Clothing-Store
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='bg-primary py-12'>
+      <div className='container mx-auto'>
+        <p className='text-white text-center'>
+          Kuanysh Muratov &copy; Clothing Store. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
+
